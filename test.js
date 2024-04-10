39037ec0f911fe2ffcd05bb54dacd40fe1e79afe
Islam Rabea <149504798+eslarabia@users.noreply.github.com>
2024-04-10 11:18:41 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,432 @@
+let span = document.querySelectorAll("span");
+let Timing = document.querySelector(".parent");
+let paragraph = document.querySelector("p");
+
+let btnThree = document.querySelector(".three");
+let btnTwo = document.querySelector(".two");
+let btnOne = document.querySelector(".one");
+let btnFour = document.querySelector(".four");
+
+let country = document.querySelector(".Country div");
+let countryImg = document.querySelector(".Country img");
+
+span[1].style.display = "none";
+span[2].style.display = "none";
+span[3].style.display = "none";
+span[4].style.display = "none";
+
+// Timer in Egypt
+
+let myFunc = function () {
+  let date = new Date();
+
+  let hour = date.getHours(); // 0 - 23
+  let minutes = date.getMinutes(); // 0 - 59
+  let second = date.getSeconds(); // 0 - 59
+  let flag = "AM";
+
+  if (hour == 0) {
+    hour = 12;
+  }
+
+  if (hour > 12) {
+    hour -= 12;
+    flag = "PM";
+  }
+
+  (hour < 10) ? hour = "0" + hour : false;
+  (minutes < 10) ? minutes = "0" + minutes : false;
+  (second < 10) ? second = "0" + second : false;
+
+  span[0].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+  setTimeout(function () {
+    myFunc();
+  }, 1000);
+};
+
+myFunc();
+
+// Date
+
+let parent = dayjs(Timing.dataset.data).format("dddd, MMM M / D / YYYY");
+paragraph.innerHTML = parent;
+
+// Timer in Saudi arabia
+
+btnThree.addEventListener("click", function () {
+  span[0].style.display = "none";
+  span[1].style.display = "none";
+  span[2].style.display = "block";
+  span[3].style.display = "none";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours() + 1; // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[2].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  country.innerHTML = "Timer In Saudi Arabia";
+  countryImg["src"] = "R.jpeg";
+});
+
+// Timer in Egypt
+
+btnOne.addEventListener("click", function () {
+  span[0].style.display = "block";
+  span[1].style.display = "none";
+  span[2].style.display = "none";
+  span[3].style.display = "none";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours(); // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[0].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  country.innerHTML = "Timer In Egypt";
+  countryImg["src"] = "علم-مصر.jpg";
+});
+
+// Timer in Palestine
+
+btnTwo.addEventListener("click", function () {
+  span[0].style.display = "none";
+  span[1].style.display = "block";
+  span[2].style.display = "none";
+  span[3].style.display = "none";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours(); // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[1].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  country.innerHTML = "Timer In Palestine";
+  countryImg["src"] = "shutterstock_1975087202-2048x1080.jpg";
+});
+
+// Timer in Al-Amarat
+
+btnFour.addEventListener("click", function () {
+  span[0].style.display = "none";
+  span[1].style.display = "none";
+  span[2].style.display = "none";
+  span[3].style.display = "block";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours() + 2; // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[3].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  country.innerHTML = "Timer In Al-Amarat";
+  countryImg["src"] = "R (2).jpeg";
+});
+
+let timer = document.querySelector(".timer");
+
+span[0].addEventListener("click", function () {
+  timer.classList.add("active");
+  span[0].style.display = "none";
+  span[2].style.display = "none";
+  span[1].style.display = "none";
+  span[3].style.display = "none";
+
+  span[4].style.display = "block";
+  span[5].style.display = "none";
+  span[6].style.display = "none";
+  span[7].style.display = "none";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours(); // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[4].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  timer.addEventListener("click", function () {
+    timer.classList.remove("active");
+    span[0].style.display = "block";
+    span[2].style.display = "none";
+    span[1].style.display = "none";
+    span[3].style.display = "none";
+  });
+});
+
+span[1].addEventListener("click", function () {
+  timer.classList.add("active");
+  span[2].style.display = "none";
+  span[0].style.display = "none";
+  span[1].style.display = "none";
+  span[3].style.display = "none";
+
+  span[3].style.display = "none";
+  span[4].style.display = "none";
+  span[5].style.display = "block";
+  span[6].style.display = "none";
+  span[7].style.display = "none";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours(); // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[5].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  timer.addEventListener("click", function () {
+    timer.classList.remove("active");
+    span[2].style.display = "none";
+    span[0].style.display = "none";
+    span[1].style.display = "block";
+    span[3].style.display = "none";
+  });
+});
+
+
+span[2].addEventListener("click", function () {
+  timer.classList.add("active");
+  span[2].style.display = "none";
+  span[0].style.display = "none";
+  span[1].style.display = "none";
+  span[3].style.display = "none";
+
+  span[5].style.display = "none";
+  span[3].style.display = "none";
+  span[4].style.display = "none";
+  span[6].style.display = "block";
+  span[7].style.display = "none";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours() + 1; // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[6].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  timer.addEventListener("click", function () {
+    timer.classList.remove("active");
+    span[2].style.display = "block";
+    span[1].style.display = "none";
+    span[0].style.display = "none";
+    span[3].style.display = "none";
+  });
+});
+
+span[3].addEventListener("click", function () {
+  timer.classList.add("active");
+  span[2].style.display = "none";
+  span[0].style.display = "none";
+  span[1].style.display = "none";
+  span[3].style.display = "none";
+
+  span[5].style.display = "none";
+  span[3].style.display = "none";
+  span[4].style.display = "none";
+  span[6].style.display = "none";
+  span[7].style.display = "block";
+
+  let myFunc = function () {
+    let date = new Date();
+
+    let hour = date.getHours() + 2; // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let second = date.getSeconds(); // 0 - 59
+    let flag = "AM";
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (hour > 12) {
+      hour -= 12;
+      flag = "PM";
+    }
+
+    (hour < 10) ? hour = "0" + hour : false;
+    (minutes < 10) ? minutes = "0" + minutes : false;
+    (second < 10) ? second = "0" + second : false;
+
+    span[7].innerHTML = `${hour} : ${minutes} : ${second}  ${flag}`;
+
+    setTimeout(function () {
+      myFunc();
+    }, 1000);
+  };
+
+  myFunc();
+
+  timer.addEventListener("click", function () {
+    timer.classList.remove("active");
+    span[2].style.display = "none";
+    span[1].style.display = "none";
+    span[0].style.display = "none";
+    span[3].style.display = "block";
+  });
+});
